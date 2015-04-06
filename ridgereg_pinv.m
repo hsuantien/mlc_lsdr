2a908a583f9ab3b7ea51f051758cac3a460287b7
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-06 22:24:58 +0800

    add some more comments

--- ridgereg_pinv.m
+++ ridgereg_pinv.m
@@ -1,3 +1,5 @@
 function invX = ridgereg_pinv(X, lambda)
-  invX = inv(X' * X + lambda * eye(size(X, 2))) * X';
+% ``pseudo-inverse'' subject to regulariztion in ridge regression
+%   needs lambda > 0
 
+  invX = inv(X' * X + lambda * eye(size(X, 2))) * X';

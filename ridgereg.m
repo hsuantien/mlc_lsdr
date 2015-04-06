3ae74d85a91a817a92e7817f66a41663a2d8e654
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-06 22:08:00 +0800

    remove special handling of theta; just use longer w

--- ridgereg.m
+++ ridgereg.m
@@ -3,18 +3,10 @@ function [G_tt] = ridgereg(y_tr, x_tr, y_tt, x_tt)
   [training_size, label_size] = size(y_tr);
   [testing_size, feature_num] = size(x_tt);
 
-  w = zeros(feature_num, label_size);
-  neg_ones = -1 * ones(training_size,1);
+  w = zeros(feature_num + 1, label_size);
+  neg_ones = -ones(training_size,1);
   hat = ((([neg_ones x_tr]')*[neg_ones x_tr] + 0.01*eye(feature_num+1))^-1)*([neg_ones x_tr]');
 
-  theta = zeros(label_size,1);
-  for i = 1:label_size
-    w_tmp = hat*y_tr(:,i);
-    theta(i) = w_tmp(1);
-    w(:,i) = w_tmp(2:length(w_tmp));
-  end
-  theta_matrix_tt = ones(testing_size,label_size);
-  for i=1:label_size 
-    theta_matrix_tt(:,i) = theta(i) * theta_matrix_tt(:,i);
-  end
-  G_tt = x_tt * w - theta_matrix_tt;
+  w(:, i) = hat * y_tr;
+
+  G_tt = [-one(testing_size, 1) x_tt] * w;

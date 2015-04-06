03d40f7e04c8a2d54dcf121e7c614cf40f00ddfc
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-06 22:15:34 +0800

    use N, K to simply denote sizes

--- ridgereg.m
+++ ridgereg.m
@@ -1,9 +1,9 @@
 function [G_tt] = ridgereg(y_tr, x_tr, y_tt, x_tt)
 
-  [training_size, label_size] = size(y_tr);
+  [N, K] = size(y_tr);
   [testing_size, feature_num] = size(x_tt);
 
-  X = [ones(training_size, 1) x_tr];
+  X = [ones(N, 1) x_tr];
   w = ridgereg_pinv(X) * y_tr;
 
   G_tt = [-one(testing_size, 1) x_tt] * w;

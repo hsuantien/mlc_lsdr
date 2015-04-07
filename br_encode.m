87893e05fbac5b39597dacfa0b86963c880daf2c
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-07 22:43:41 +0800

    y_tr and y_tt to Y and Yt

--- br_encode.m
+++ br_encode.m
@@ -1,11 +1,11 @@
-function [Ytr, Ytt, perm] = br_encode(y_tr, y_tt, m)
+function [Ytr, Ytt, perm] = br_encode(Y, Yt, m)
 
-[training_size, label_size]=size(y_tr);
+[training_size, label_size]=size(Y);
 perm1 = 1:label_size;
 perm2 = randperm(label_size);
 perm = [perm1; perm2];
-Ytr = y_tr(:, perm2);
-Ytt = y_tt(:, perm2);
+Ytr = Y(:, perm2);
+Ytt = Yt(:, perm2);
 Ytr=Ytr(:,1:m);
 Ytt=Ytt(:,1:m);
 

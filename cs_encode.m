5e8113a16188e26eb1accb5a42943cb9a54dba61
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-08 05:53:16 +0800

    update prototype

--- cs_encode.m
+++ cs_encode.m
@@ -1,5 +1,5 @@
-function [Z, Zt, A] = compress_hadamard(Y, Yt, m, FilePath)
-%dimension reduction: hadamard
+function [Z, Zt, A] = cs_encode(Y, Yt, M)
+
 [training_size, label_size]=size(Y);
 hadamard_n=2^(ceil(log2(label_size)));
 A = generate_rand_hadamard(m,label_size,hadamard_n);

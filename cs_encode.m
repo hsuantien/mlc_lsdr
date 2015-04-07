fcdf2a4a3a285e16280fcea24df98566e31d0d6d
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-08 05:54:14 +0800

    simplify computation of Z and Zt

--- cs_encode.m
+++ cs_encode.m
@@ -3,8 +3,6 @@ function [Z, Zt, A] = cs_encode(Y, Yt, M)
 [training_size, label_size]=size(Y);
 hadamard_n=2^(ceil(log2(label_size)));
 A = generate_rand_hadamard(m,label_size,hadamard_n);
-Z=A*Y';
-Zt=A*Yt';
-Z=Z';
-Zt=Zt';
+Z = Y * A';
+Zt = Yt * A';
 

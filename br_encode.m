27dd98653ad8d2c2bca97e9c7e1397bd39283a01
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-07 22:49:03 +0800

    fix a bug in randperm

--- br_encode.m
+++ br_encode.m
@@ -1,8 +1,7 @@
 function [Z, Zt, Vm] = br_encode(Y, Yt, M)
-
   [N, K] = size(Y);
   Vm = speye(K);
-  idx = randperm(1:K);
+  idx = randperm(K);
   Vm = Vm(idx, 1:M);
   Z = Y * Vm;
   Zt = Yt * Vm;

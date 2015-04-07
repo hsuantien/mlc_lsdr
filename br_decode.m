204057aa1faff4a485ba1946256c55e85b070b38
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-07 22:50:25 +0800

    rewrite br_decode.m with one matrix computation line

--- br_decode.m
+++ br_decode.m
@@ -1,9 +1,4 @@
-function [G_tt] = br_decode(G_prime, perm, label_size)
+function [Y_pred] = br_decode(Z_pred, Vm)
 
-%reconstruction: add zeros or negative ones
-[testing_size, m]=size(G_prime);
-G_tt=[G_prime -1*ones(testing_size, label_size-m)];
-[permed, inverse_perm]=sort(perm(2,:));
-G_tt=G_tt(:, inverse_perm);
+Y_pred = Z_pred * Vm';
 
-return

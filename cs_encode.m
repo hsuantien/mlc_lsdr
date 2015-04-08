3e92a7fd89842b1bdec499b06a54216704b55ba7
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-08 08:44:42 +0800

    use Kh for hadamard size

--- cs_encode.m
+++ cs_encode.m
@@ -1,15 +1,15 @@
 function [Z, Zt, A] = cs_encode(Y, Yt, M)
 
   [N, K]=size(Y);
-hadamard_n=2^(ceil(log2(K)));
-A = generate_rand_hadamard(m,K,hadamard_n);
+  Kh = 2^(ceil(log2(K)));
+A = generate_rand_hadamard(m,K, Kh);
 Z = Y * A';
 Zt = Yt * A';
 
-function [A]=generate_rand_hadamard(m,K,hadamard_size)
+function [A]=generate_rand_hadamard(m,K,Kh)
 A=[];
-H=hadamard(hadamard_size);
-p=randperm(hadamard_size);
+H=hadamard(Kh);
+p=randperm(Kh);
 
 for i=1:1:m
     A=[A; H(p(i),1:K)];

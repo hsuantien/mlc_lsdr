a4bb69b8276b0bd6e8bc302c0f199c574dc5771c
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-08 08:41:25 +0800

    copy paste generate_random_hadamard from Farbound

--- cs_encode.m
+++ cs_encode.m
@@ -6,3 +6,13 @@ A = generate_rand_hadamard(m,label_size,hadamard_n);
 Z = Y * A';
 Zt = Yt * A';
 
+function [A]=generate_rand_hadamard(m,d,hadamard_size)
+A=[];
+H=hadamard(hadamard_size);
+p=randperm(hadamard_size);
+
+for i=1:1:m
+    A=[A; H(p(i),1:d)];
+end
+return
+

ff70e2964811e7bb1f4ef82626007ce2bbd20731
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-04-08 08:44:06 +0800

    use K for label size

--- cs_encode.m
+++ cs_encode.m
@@ -1,18 +1,18 @@
 function [Z, Zt, A] = cs_encode(Y, Yt, M)
 
-[training_size, label_size]=size(Y);
-hadamard_n=2^(ceil(log2(label_size)));
-A = generate_rand_hadamard(m,label_size,hadamard_n);
+  [N, K]=size(Y);
+hadamard_n=2^(ceil(log2(K)));
+A = generate_rand_hadamard(m,K,hadamard_n);
 Z = Y * A';
 Zt = Yt * A';
 
-function [A]=generate_rand_hadamard(m,d,hadamard_size)
+function [A]=generate_rand_hadamard(m,K,hadamard_size)
 A=[];
 H=hadamard(hadamard_size);
 p=randperm(hadamard_size);
 
 for i=1:1:m
-    A=[A; H(p(i),1:d)];
+    A=[A; H(p(i),1:K)];
 end
 return
 

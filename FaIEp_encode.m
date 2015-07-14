7e59b7fd2504f7a75c0159db1cf21009961db5c7
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2015-07-14 22:45:36 +0800

    remove D1 for simplicity

--- FaIEp_encode.m
+++ FaIEp_encode.m
@@ -3,9 +3,7 @@ function [Z, recover] = FaIEp_encode(X, Y, M, lambda, alpha)
         alpha = 0.1;
     end
 
-    D1 = Y * Y';
     Delta = ridgereg_hat(X, lambda);
-
-    Omega = D1 + alpha * Delta;
+    Omega = Y * Y' + alpha * Delta;
     [Z, D] = eigs(Omega, M);
     recover = Z' * Y;
